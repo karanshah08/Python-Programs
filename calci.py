b43c9b03988d956973f1bdce5ddc4bef80c00df4
karanshah08 <98231930+karanshah08@users.noreply.github.com>
2022-03-27 10:38:49 +0530

    Add files via upload

---
+++ calci.py
@@ -0,0 +1,18 @@
+a = int(input("Enter a :"))
+b = int(input("Enter b :"))
+operator = input("Select an operator (+,-,/,*,%) : ")
+
+if operator == "+":
+    print(a+b)
+elif operator == "-":
+    print(a-b)    
+elif operator == "/":
+    print(a/b) 
+elif operator == "*":
+    print(a*b)
+elif operator == "%":
+    print(a%b) 
+else:
+    print("Invalid Operator")
+
+print("The above is your answer")              
